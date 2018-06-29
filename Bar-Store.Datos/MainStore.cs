80c9b6c2fca58ad945cf5039aa1695153aa7229e
Miguel Malagon <miguelmalagon@outlook.com>
2018-06-29 11:05:45 -0500

    add projects

---
+++ Bar-Store.Datos/MainStore.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bar_Store.Datos
+{
+    class MainStore
+    {
+        private string ConectionString = Properties.Resources.cad;
+        
+    }
+}
